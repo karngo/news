5a63a043028a035f942799eaed2910e4a1f2ff41
Karna Gogoi <karnagogoi7@gmail.com>
2025-03-07 13:06:30 +0530

    Add news title and description

--- app/src/main/java/com/example/news/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/news/ui/screens/home/HomeScreen.kt
@@ -1,5 +1,11 @@
 package com.example.news.ui.screens.home
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -8,7 +14,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.news.ui.theme.NewsTheme
 
 @Composable
@@ -20,14 +29,22 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         homeViewModel.getNews()
     }
 
-    LazyColumn(modifier = modifier) {
+    LazyColumn(modifier = modifier.padding(12.dp)) {
         items(news) { article ->
-            Text(text = article.title ?: "")
+            Column(
+                modifier = Modifier
+                    .border(BorderStroke(1.dp, SolidColor(Color.Blue)))
+                    .padding(12.dp)
+            ) {
+                Text(text = article.title ?: "", fontWeight = FontWeight.Bold)
+                Text(text = article.description ?: "")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun HomePreview() {
     NewsTheme {
